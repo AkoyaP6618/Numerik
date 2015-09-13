e964f59009c8cf92ffea7a194a47df68a00ef3eb
Matthias <matthias.thurow@outlook.com>
2015-09-13 14:24:00 +0200

    test awp :)

--- Numerik/src/numerik/cs13/Launcher.java
+++ Numerik/src/numerik/cs13/Launcher.java
@@ -2,48 +2,49 @@ package numerik.cs13;
 
 public class Launcher {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+	rungeKutta();
+    }
+
+    // aendern
+    public static void rungeKutta() {
+
+	double y = 2;
+	double x = 0;
+	double h = 0.1;
+
+	double k1 = 0;
+	double k2 = 0;
+	double k3 = 0;
+	double k4 = 0;
+
+	// i aendern!
+	for (int i = 0; i <= 5; i++) {
+
+	    // x = xi = ?
+	    k1 = f(x, y);
+	    k2 = f(x + 0.5 * h, y + 0.5 * k1 * h);
+	    k3 = f(x + 0.5 * h, y + 0.5 * k2 * h);
+	    k4 = f(x + h, y + k3 * h);
+	    y = y + 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4) * h;
+	    x = x + h;
+
+	    System.out.println("Schritt " + i + ": " + x + " ; " + y);
 
 	}
 
-	
-	// aendern
-	public static void rungeKutta(){
-		
-		double y = 0;
-		double x = 0;
-		double h = 0.1;
-		
-		double k1 = 0;
-		double k2 = 0;
-		double k3 = 0;
-		double k4 = 0;
-		
-		// i aendern!
-		for(int i = 0; i == 0; i++){
+	// 		Formel Runge-Kutta: 
+	//		yn+1 = yn + 1/6 (k1 + 2 * k2 + 2 * k3 + k4) h
+	//			k1 = f(xi, yi)
+	//			k2 = f(xi + 1/2 * h, yi + 1/2 * k1 * h)
+	//			k3 = f(xi + 1/2 * h, yi + 1/2 * k2 * h)
+	//			k4 = f(xi + h, yi + k3 * h)
 
-			// x = xi = ?
-			k1 = f(x, y);
-			k2 = f(x + 0.5*h, y + 0.5*k1*h);
-			k3 = f(x + 0.5*h, y + 0.5*k2*h);
-			k4 = f(x + h, y + k3*h);
-			y = y + 1/6 * (k1 + 2 * k2 + 2 * k3 + k4 ) * h;
-			
-		}
-	
-// 		Formel Runge-Kutta: 
-//		yn+1 = yn + 1/6 (k1 + 2 * k2 + 2 * k3 + k4) h
-//			k1 = f(xi, yi)
-//			k2 = f(xi + 1/2 * h, yi + 1/2 * k1 * h)
-//			k3 = f(xi + 1/2 * h, yi + 1/2 * k2 * h)
-//			k4 = f(xi + h, yi + k3 * h)
-		
-		
-	}
-	
-	public static double f(double x, double y){
-		// Definiere funktion hier!
-		return y - x;
-	}
-	
+    }
+
+    public static double f(double x, double y) {
+	// Definiere funktion hier!
+	return y - Math.pow(x, 3);
+    }
+
 }
